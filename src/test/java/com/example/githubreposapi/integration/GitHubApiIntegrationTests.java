13e8dc16fe9b680126453b2735d5bea7126d5539
Sonia Becz <sonia.becz@onet.pl>
2025-04-03 13:24:01 +0200

    first app version

--- src/test/java/com/example/githubreposapi/integration/GitHubApiIntegrationTests.java
+++ src/test/java/com/example/githubreposapi/integration/GitHubApiIntegrationTests.java
@@ -1,4 +1,4 @@
-package com.example.githubreposapi;
+package com.example.githubreposapi.integration;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
